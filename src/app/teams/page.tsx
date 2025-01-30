fb7fcd20da5e1f91a21260b697cd0d3fd85804a3
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2025-01-30 23:56:09 +0530

    Team Card Added

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -6,14 +6,19 @@ import teamCol from "@/components/columns/TeamCol";
 // import { user } from "@/store/interfaces";
 import tooms from "@/components/dumTeams.json";
 // import useToast from "@/lib/toast";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { type Team } from "@/data/schema";
 import { fetchTeams } from "@/api/fetchTeams";
 import { useState } from "react";
+import { TeamModal } from "@/components/table/team-modal";
 
 export default function Teams() {
   const [pageIndex, setPageIndex] = useState(0);
   const [pageSize, setPageSize] = useState(10);
+  const [selectedTeam, setSelectedTeam] = useState<Team | null>(null);
+  const [open, setOpen] = useState(false);
+
+  const queryClient = useQueryClient();
 
   const {
       data: teamList,
@@ -24,17 +29,37 @@ export default function Teams() {
       queryFn: () => fetchTeams({ page: pageIndex + 1, limit: pageSize }),
   });
 
+  const handlePageChange = (page: number)=>{
+    setPageIndex(page);
+    queryClient.invalidateQueries({queryKey: ["teams"]});
+  }
+
+  const handlePageSizeChange = (size: number)=>{
+    setPageSize(size);
+    queryClient.invalidateQueries({queryKey: ["teams"]});
+  }
+
   if (isLoading) {
     <>loading...</>;
   }
   if (isError) {
     <>skill issue</>;
   }
+  const handleRowClick = (team: Team) => {
+    setSelectedTeam(team);
+    setOpen(true);
+  };
+
+
+  const handleModalClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <div className="p-4">
         <div className="mb-4"></div>
         {/* <DataTableUsers users={oosers} columns={userCol} /> */}
+        {selectedTeam && <TeamModal open = {open} onClose = {handleModalClose} team = {selectedTeam}/>}
         <DataTable<Team, string>
             columns={teamCol}
             data={teamList?.teams ?? []}
@@ -43,6 +68,7 @@ export default function Teams() {
             onPageSizeChange={setPageSize}
             currentPage = {pageIndex}
             pageSize = {pageSize}
+            onRowClick={handleRowClick}
         />
       </div>
     </>
